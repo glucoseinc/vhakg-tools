8cdb23b483cc547da812ffb3d3f1edc759b93fc7
wf664 <kaname.abe@glucose.jp>
2024-11-07 14:05:14 +0900

    Update app/src/index.tsx
    Co-authored-by: fumimi23 <122011985+fumimi23@users.noreply.github.com>

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: 'action-object-search',
+    path: '/action-object-search',
     element: <ActionObjectSearch />,
   },
 ]);
