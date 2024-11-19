a4b19244117707e03dcd6c940ca78ea6f53695db
Kaname Abe <kaname.abe@glucose.jp>
2024-11-19 16:40:28 +0900

    Sceneをクエリパラメータが存在する場合それで初期化するように変更

--- app/src/action_object_search/ActionObjectSearch.tsx
+++ app/src/action_object_search/ActionObjectSearch.tsx
@@ -22,6 +22,7 @@ import {
   VIDEO_DURATION_KEY,
   TARGET_OBJECT_KEY,
   TOTAL_VIDEOS_PER_PAGE,
+  SCENE_KEY,
 } from 'action_object_search/constants';
 import {
   type ActionQueryType,
@@ -61,7 +62,9 @@ function ActionObjectSearch(): React.ReactElement {
   const [searchResultPage, setSearchResultPage] = useState<number>(
     Number(searchParams.get(SEARCH_RESULT_PAGE_KEY)) || 1
   );
-  const [selectedScene, setSelectedScene] = useState<string>('');
+  const [selectedScene, setSelectedScene] = useState<string>(
+    searchParams.get(SCENE_KEY) || ''
+  );
 
   const handleSearchParamsChange = useCallback(
     (key: SearchParamKey, value: string) => {
