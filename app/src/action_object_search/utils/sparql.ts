2fe1904e7b3fb0db7f3dd4f378139744e036d4c6
Kaname Abe <kaname.abe@glucose.jp>
2024-11-21 11:35:14 +0900

    videoSegmentを取得するSPARQLクエリの作成

--- app/src/action_object_search/utils/sparql.ts
+++ app/src/action_object_search/utils/sparql.ts
@@ -60,6 +60,60 @@ export const fetchVideo: (
   return result;
 };
 
+export type VideoSegmentQueryType = {
+  videoSegment: NamedNode;
+  base64Video: NamedNode;
+  frameRate: NamedNode;
+  startFrame: NamedNode;
+  endFrame: NamedNode;
+};
+export const fetchVideoSegment: (
+  action: string,
+  mainObject: string,
+  targetObject: string,
+  scene: string,
+  camera: string,
+  limit: number,
+  page: number
+) => Promise<VideoSegmentQueryType[]> = async (
+  action,
+  mainObject,
+  targetObject,
+  scene,
+  camera,
+  limit,
+  page
+) => {
+  const query = `
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX mssn: <http://mssn.sigappfr.org/mssn/>
+    PREFIX vh2kg: <http://kgrc4si.home.kg/virtualhome2kg/ontology/>
+    SELECT DISTINCT ?videoSegment ?base64Video ?frameRate ?startFrame ?endFrame WHERE {
+      ?mainObject rdfs:label ?mainObjectLabel FILTER regex(?mainObjectLabel, "${mainObject}", "i") .
+      ${
+        targetObject !== ''
+          ? `?targetObject rdfs:label ?targetObjectLabel FILTER regex(?targetObjectLabel, "${targetObject}", "i") .`
+          : ''
+      }
+      ?event vh2kg:mainObject ?mainObject ;
+             ${targetObject !== '' ? 'vh2kg:targetObject ?targetObject ;' : ''}
+             vh2kg:action <${action}> ;
+             vh2kg:hasVideoSegment ?videoSegment .
+      ?videoSegment vh2kg:hasStartFrame ?startFrame ;
+                    vh2kg:hasEndFrame ?endFrame .
+      ?camera mssn:hasMediaSegment ?videoSegment ;
+              vh2kg:video ?base64Video ;
+              vh2kg:frameRate ?frameRate .
+      ${scene !== '' ? `FILTER regex(STR(?camera), "${scene}", "i") .` : ''}
+      ${camera !== '' ? `FILTER regex(STR(?camera), "${camera}", "i") .` : ''}
+    } ORDER BY asc(?videoSegment) LIMIT ${limit} OFFSET ${limit * (page - 1)}`;
+
+  const result = (await makeClient().query.select(
+    query
+  )) as VideoSegmentQueryType[];
+  return result;
+};
+
 export type VideoCountQueryType = {
   videoCount: NamedNode;
 };
