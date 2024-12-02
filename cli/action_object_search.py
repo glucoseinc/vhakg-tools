10bed681a24f394da6d24cdb17c14a808c798ec9
Kaname Abe <kaname.abe@glucose.jp>
2024-12-02 13:40:55 +0900

    コマンドライン引数cameraの型を修正,コマンドライン引数の単語区切りをハイフンで統一

--- cli/action_object_search.py
+++ cli/action_object_search.py
@@ -6,11 +6,11 @@ def main():
     args = get_args()
 
     action: str = args.action
-    main_object: str = args.main_object
+    main_object: str = args.__getattribute__('main-object')
     target_object: str = args.target_object
     camera_number: int = args.camera
     is_segment: bool = args.segment
-    output_path: str = args.output_path
+    output_path: str = args.__getattribute__('output-path')
 
     absolute_output_path = Path(output_path).resolve()
 
@@ -21,11 +21,11 @@ def get_args():
     )
 
     parser.add_argument("action", type=str, help="The action to search for (exact matching)")
-    parser.add_argument("main_object", type=str, help="The main object of an event (partially matching)")
+    parser.add_argument("main-object", type=str, help="The main object of an event (partially matching)")
     parser.add_argument("-t", "--target-object", type=str, help="The target object of an event (partially matching)")
-    parser.add_argument("-c",  "--camera",  type=int, help="The camera number to search for (optional)")
+    parser.add_argument("-c",  "--camera",  type=str, help="The camera number to search for (optional)")
     parser.add_argument("-s", "--segment", action='store_true', help="The flag to search for the segments of the videos")
-    parser.add_argument("output_path", type=str, help="The directory to save the search results (can be relative or absolute)")
+    parser.add_argument("output-path", type=str, help="The directory to save the search results (can be relative or absolute)")
 
     return parser.parse_args()
 
