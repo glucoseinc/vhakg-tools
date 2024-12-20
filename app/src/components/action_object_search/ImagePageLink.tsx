9ea7303105249df3b3e9e7bc741e95031d1bfa63
Kaname Abe <kaname.abe@glucose.jp>
2024-12-20 16:23:59 +0900

    検索結果の動画名が長い場合に、テキストを折り返すように変更

--- app/src/components/action_object_search/ImagePageLink.tsx
+++ app/src/components/action_object_search/ImagePageLink.tsx
@@ -7,6 +7,25 @@ import {
 } from 'constants/action_object_search/constants';
 import React from 'react';
 
+function splitVideoName(videoName: string) {
+  const pattern = /(.+)_(scene\d+)_(camera\d+)/;
+  const groups = pattern.exec(videoName)?.slice(1);
+  if (!groups) {
+    return videoName;
+  }
+  return groups.join(' ');
+}
+
+function splitVideoSegmentName(videoSegmentName: string) {
+  const pattern = /(.+)_(\d+)_(scene\d+)_(video_segment\d+)/;
+  const groups = pattern.exec(videoSegmentName)?.slice(1);
+  if (!groups) {
+    return videoSegmentName;
+  }
+  groups[1] = 'camera' + groups[1];
+  return groups.join(' ');
+}
+
 type ImagePageLinkProps = {
   linkText: string;
   mainObject: string;
@@ -27,8 +46,10 @@ export function ImagePageLink({
   const path =
     '/action-object-search/2d-bbox-image?' + imageSearchParams.toString();
   return (
-    <Link as={ReactRouterLink} to={path}>
-      {linkText}
+    <Link as={ReactRouterLink} to={path} overflowWrap={'break-word'}>
+      {linkText.includes('video_segment')
+        ? splitVideoSegmentName(linkText)
+        : splitVideoName(linkText)}
     </Link>
   );
 }
